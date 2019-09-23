bcee5641833a07c9e70628871d3271b8ea4923fa
SiZiOUS <sizious@gmail.com>
2019-09-23 20:24:16 +0200

    Displaying the WSAGetLastError message.

--- host-src/tool/utils.c
+++ host-src/tool/utils.c
@@ -9,7 +9,11 @@ void log_error( const char * prefix ) {
 #ifdef __MINGW32__	
 	DWORD dwError = WSAGetLastError();
 	if ( dwError ) {
-      printf("WSAGetLastError: %d\n", dwError);
+		TCHAR *err = NULL;
+		FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_MAX_WIDTH_MASK, 
+			NULL, dwError, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&err, 0, NULL);
+		fprintf(stderr, "WSAGetLastError: %d / %s\n", dwError, err);
+		LocalFree(err);
 	}
 #endif
 }
